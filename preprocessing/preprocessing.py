d74189814f562c2bfef3c384c23a345892a41121
Kaya Meriç Engin <mericengin@hotmail.com>
2025-04-16 15:50:09 +0300

    feat: :sparkles: Added a preprocessing.py file and folder and implemented the preprocessing function in this folder's file.

---
+++ preprocessing/preprocessing.py
@@ -0,0 +1,25 @@
+import re 
+import nltk
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+
+def preprocess_text(text):
+    """This function preprocesses text (removing html tags, removing stopwords, lowercasing and tokenizing the texts)"""
+    
+    # Remove HTML Tags
+    text = re.sub(r'<.*?>', '', text)
+    
+    # Tokenize Text
+    tokens = word_tokenize(text)
+    
+    # Lowercase Everything
+    tokens = [word.lower() for word in tokens]
+    
+    # Remove Newlines
+    tokens = [word.strip() for word in tokens]
+    
+    # Remove Stopwords
+    stop_words = set(stopwords.words('english'))
+    filtered_tokens = [word for word in tokens if word.isalph() and word not in stop_words]
+    
+    return filtered_tokens
